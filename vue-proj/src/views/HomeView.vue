f35b4a686f37e91c8711d84a564e1baf35d7b2e1
AliceLin321 <ataechiml24@gmail.com>
2023-05-16 13:59:56 -0400

    trh
    ftyh

--- vue-proj/src/views/HomeView.vue
+++ vue-proj/src/views/HomeView.vue
@@ -1,8 +1,16 @@
 <template>
-<div>th rthh
-</div>
-</template> 
+    <div>
+    </div>
+</template>
 <script setup>
+import {ref, OnMounted} from 'vue'
+const pokemon = ref('') 
+async function getPokemon() {
+let res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+let data = await res.json()
+pokemon.value = data.results
+}
+OnMounted (() => {
+    getPokemon()
+})
 </script>
-<style  scoped>
-</style>
