0af985e495d55ef815a80090547542aa3f666946
AliceLin321 <ataechiml24@gmail.com>
2023-05-23 13:54:08 -0400

    gre
    gr

---
+++ vue-proj/src/views/barchartview.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+      <h3> NYPD Shooting Incident Data (Year To Date) </h3>
+      <BarChart></BarChart>
+    </div>
+  </template>
+ 
+  <script setup>
+  import { ref, onMounted } from 'vue'
+  import BarChart from '../components/BarChart.vue'
+  const incidents = ref('')
+  async function getIncidents() {
+    let res = await fetch('https://data.cityofnewyork.us/resource/5ucz-vwe8.json')
+    let data = await res.json()
+    console.log(data)
+  }
+  onMounted(async () => {
+    await getIncidents()
+  })
+  </script>
+  <style scoped>
+  .container {
+    width: 60vw;
+    margin: 20px auto;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space around;
+  }
+  </style>
