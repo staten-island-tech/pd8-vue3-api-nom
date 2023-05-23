f8e34c0c471f30fd8f7ed4aeeafa6bb962472553
AliceLin321 <ataechiml24@gmail.com>
2023-05-23 13:56:50 -0400

    gfhf
    th

--- vue-proj/src/views/HomeView.vue
+++ vue-proj/src/views/HomeView.vue
@@ -1,7 +1,16 @@
 <template>
-<div></div>
-</template> 
-<script setup>
-</script>
-<style  scoped>
-</style>
+    <div class="home">
+      <h1>This is a home page</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .home {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
