4a63498c91769b48083500e6b59f38fc833f9035
AliceLin321 <ataechiml24@gmail.com>
2023-06-08 18:30:18 -0400

    j
    j

--- vue-proj/src/views/HomeView.vue
+++ vue-proj/src/views/HomeView.vue
@@ -1,22 +1,14 @@
 <template>
-    <!-- <div class="home">
-      <h1>This is a home page</h1>
-      <PokemonCard
-      v-for="(monster, index) in pokemon"
-      :key="monster.name"
-      :id="index + 1"
-      :pokemon="monster"
-      />
-    </div> -->
-  </template>
-  
-  <style>
-  @media (min-width: 1024px) {
-    .home {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
-  }
-  </style>
-  
+<div>
+
+</div>
+</template>
+
+<script setup>
+import {ref, onMounted} from 'vue'
+const pokemon = ref('')
+</script>
+
+<script lang="scss" scoped>
+
+</script>
