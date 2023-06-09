75ff03ec41730c331cefbc93e810da1e6facf666
AliceLin321 <ataechiml24@gmail.com>
2023-06-09 00:05:10 -0400

    fe
    sef

--- vue-proj/src/components/PokemonCard.vue
+++ vue-proj/src/components/PokemonCard.vue
@@ -14,6 +14,10 @@ const props = defineProps({
     pokemon: Object,
     id: Number
 })
+const pkmnPath = computed(() => {
+    return `/PokemonData/${props.pokemon.name}`
+}
+)
 </script>
 
 <style scoped>
