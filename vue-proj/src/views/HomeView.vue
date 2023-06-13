62c749284d175c9a25a8d3cec44283c77a4b07b7
AliceLin321 <ataechiml24@gmail.com>
2023-06-13 13:45:46 -0400

    se
    fef

--- vue-proj/src/views/HomeView.vue
+++ vue-proj/src/views/HomeView.vue
@@ -1,34 +1,49 @@
 <template>
-<div class="container">
-    <PokemonCard v-for="(monster, index) in pokemon" 
-    :key="monster.name" 
-    :id="index + 1" 
-    :pokemon="monster"
-    />
-</div>
-</template>
+    <div class="container">
+        <PokemonCard v-for="(monster, index) in pokemon"
+        :key="monster.name"
+        :id="index + 1"
+        :pokemon="monster"
+        />
+    </div>
+    <barChart v-if="pokemon" :data="pokemon"></barChart>
+    <pieChart v-if="pokemon" :data="pokemon"></pieChart>
 
-<script setup>
-import {ref, onMounted} from 'vue'
-import PokemonCard from '../components/PokemonCard.vue'
-const pokemon = ref('')
-async function getPokemon() {
-    let res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
-let data = await res.json()
-pokemon.value = data.results
-}
-onMounted(() => {
-    getPokemon()
-})
-</script>
-<style scoped>
-.container {
-    width: 80vw;
-    margin: 30px auto;
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-around;
-}
-</style>
+    </template>
+    
+    
+    <script setup>
+    import {ref, onMounted} from 'vue'
+    import barChart from '../components/barChart.vue';
+    import pieChart from '../components/pieChart.vue';
+
+    import PokemonCard from '../components/PokemonCard.vue'
+    const pokemon = ref(false)
+    async function getPokemon() {
+        let res = await fetch('https://data.cityofnewyork.us/resource/5ucz-vwe8.json')
+    let data = await res.json()
+    let newData = [
+        ["Manhatan",data.filter(sh=>sh.boro=="MANHATTAN").length],
+        ["Staten Island",data.filter(sh=>sh.boro=="STATEN ISLAND").length],
+        ["Bronx",data.filter(sh=>sh.boro=="BRONX").length],
+        ["Brooklyn",data.filter(sh=>sh.boro=="BROOKLYN").length],
+        ["Queens",data.filter(sh=>sh.boro=="QUEENS").length],
+    ]
+    console.log(newData)
+    pokemon.value = newData
+    }
+    onMounted(() => {
+        getPokemon()
+    })
+    </script>
+    <style scoped>
+    .container {
+        width: 80vw;
+        margin: 30px auto;
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-around;
+    }
+    </style>
